84a3a795f9c5d8455f6a411f5640e4b83b2134d4
Heng Mengsroin <heng.mengsroin@optistech.com>
2022-12-06 10:29:29 +0700

    test cicd

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Hello world');
+  return Response(body: 'Hello world tested');
 }
